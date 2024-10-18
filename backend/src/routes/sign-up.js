0e1b447d489a3c0182f827abc03baff34499d528
JoshCodedit <joshua.dixoncms@gmail.com>
2024-10-18 16:17:28 +0100

    feat: login added but not yet tested

--- backend/src/routes/sign-up.js
+++ backend/src/routes/sign-up.js
@@ -4,27 +4,35 @@ const bcrypt = require("bcryptjs");
 
 function get(req, res) {
   const title = "Create an account";
-  const content = ""; //this will be added once backend is finished;
+  const content = ""; // This will be added once backend is finished;
   const body = Layout({ title, content });
   res.send(body);
 }
 
-function post(req, res) {
+async function post(req, res) {
   const { email, password } = req.body;
+
   if (!email || !password) {
-    res.status(400).send("Bad input");
-  } else {
-    bcrypt.hash(password, 12).then((hash) => {
-      const user = createUser(email, hash);
-      const session_id = createSession(user.id);
-      res.cookie("sid", session_id, {
-        signed: true,
-        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-        sameSite: "lax",
-        httpOnly: true,
-      });
-      res.redirect(`/confessions/${user.id}`);
+    return res.status(400).send("Bad input");
+  }
+
+  try {
+    const hash = await bcrypt.hash(password, 12);
+
+    const user = await createUser(email, hash);
+    const session_id = await createSession(user.id);
+
+    res.cookie("sid", session_id, {
+      signed: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      sameSite: "lax",
+      httpOnly: true,
     });
+
+    return res.redirect(`/confessions/${user.id}`);
+  } catch (error) {
+    console.error("Error during user creation:", error);
+    return res.status(500).send("Internal server error");
   }
 }
 
