5952adb759209362c998befea0a01f7ef0261273
JoshCodedit <joshua.dixoncms@gmail.com>
2024-11-20 11:04:18 +0000

    fix: navbar background colour

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function Nav() {
   return (
     <div className="flex flex-col h-screen w-64 bg-user-dash-main text-white">
-      <nav className="flex flex-col mt-4">
+      <nav className="flex flex-col mt-4 ">
         <NavLink
           to="/dashboard"
           className={({ isActive }) =>
